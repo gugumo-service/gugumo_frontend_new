20e27800a8bea877d89793f9ca277928000fddc8
Kim Ji Yu <spbabo97@naver.com>
2024-06-24 04:21:32 +0900

    :recycle: 마이페이지 스켈레톤 UI

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -2,6 +2,9 @@
 import Wrap from "@/components/Common/Wrap";
 import Nickname from "@/components/page/auth/mypage/Nickname";
 import Password from "@/components/page/auth/mypage/Password";
+import SkeletonNickname from "@/components/page/auth/mypage/SkeletonUI/SkeletonNickname";
+import SkeletonPassword from "@/components/page/auth/mypage/SkeletonUI/SkeletonPassword";
+import SkeletonUser from "@/components/page/auth/mypage/SkeletonUI/SkeletonUser";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -10,6 +13,7 @@ export default function Mypage() {
 
     const [nickname,setNickname] = useState('');
     const {data : session} = useSession() as any;
+    const [isLoading,setIsLoading] = useState(true);
 
     const fetchs = async ()=>{
         try {
@@ -20,8 +24,8 @@ export default function Mypage() {
             });
             if(res.ok){
                 const data = await res.json();
-                console.log(data);
                 setNickname(data.data.nickname);
+                setIsLoading(false);
             }
         }
         catch(err){
@@ -42,22 +46,45 @@ export default function Mypage() {
             <h1 className="text-OnBackground text-lg md:text-2xl font-medium mt-[10px]">마이페이지</h1>
 
             <div className="flex flex-col md:flex-row items-center gap-6 md:gap-7 mt-4 md:mt-[60px]">
-                <div className="size-[78px] md:size-[104px] rounded-full border bg-center bg-[url(/asset/image/user/user.png)] bg-[length:95%_95%] bg-no-repeat"></div>
-                <div className="flex items-center gap-[7px] text-base font-medium">
-                    닉네임
-                    <p className="text-[13px] font-medium text-OnSurface border border-OnSurface py-1 px-2 rounded-full leading-none">
-                        {nickname}
-                    </p>
-                </div>
+                {
+                    isLoading 
+                    ?
+                        <SkeletonUser/>
+                    :
+                        <>
+                            <div className="size-[78px] md:size-[104px] rounded-full border bg-center bg-[url(/asset/image/user/user.png)] bg-[length:95%_95%] bg-no-repeat"></div>
+                            <div className="flex items-center gap-[7px] text-base font-medium">
+                                닉네임
+                                <p className="text-[13px] font-medium text-OnSurface border border-OnSurface py-1 px-2 rounded-full leading-none">
+                                    {nickname}
+                                </p>
+                            </div>
+                        </>
+                }
             </div>
         </Wrap>
 
         <Wrap className="border-t-[6px] border-Surface md:border-0 mt-10">
-            <Nickname setNickname={setNickname}/>
-            <Password/>
-            <div className="text-center mt-[88px] md:mt-20">
-                <button className="text-xs md:text-base font-medium cursor-pointer border-b border-OnBackgroundGray px-1 pb-[2px] text-OnBackgroundGray">회원탈퇴</button>
-            </div>
+            {
+                isLoading 
+                ?
+                    <SkeletonNickname/>
+                :
+                    <Nickname setNickname={setNickname}/>
+            }
+            {
+                isLoading
+                ?
+                    <SkeletonPassword/>
+                :
+                    <Password/>
+            }
+            {
+                !isLoading &&
+                    <div className="text-center mt-[88px] md:mt-20">
+                        <button className="text-xs md:text-base font-medium cursor-pointer border-b border-OnBackgroundGray px-1 pb-[2px] text-OnBackgroundGray">회원탈퇴</button>
+                    </div>
+            }
         </Wrap>
     </main>
   )
---
+++ src/components/page/auth/mypage/SkeletonUI/SkeletonUser.tsx
@@ -0,0 +1,8 @@
+export default function SkeletonUser() {
+  return (
+    <>
+        <div className="size-[78px] md:size-[104px] rounded-full bg-slate-200 animate-pulse"></div>
+        <div className="w-20 h-5 rounded bg-slate-200 animate-pulse"></div>
+    </>
+  )
+}
