4b5395a78c64860a983d1d76fced08f7c848d485
Kim Ji Yu <spbabo97@naver.com>
2024-06-23 16:31:02 +0900

    :sparkles: 등록 기능 추가

--- src/app/(auth)/post/write/page.tsx
+++ src/app/(auth)/post/write/page.tsx
@@ -1,11 +1,16 @@
 import Wrap from "@/components/Common/Wrap";
 import Form from "@/components/page/post/write/Form";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
 
-export default function Write() {
+export default async function Write() {
+
+  const session = await getServerSession(authOptions) as any;
+
   return (
     <main className="pt-8 md:pt-[90px] pb-14 md:pb-36">
       <Wrap>
-        <Form/>
+        <Form session={session}/>
       </Wrap>
     </main>
   )
