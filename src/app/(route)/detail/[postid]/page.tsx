2ba911650116c309ebdf257ad96f4a44af10d8d0
Kim Ji Yu <spbabo97@naver.com>
2024-06-30 07:06:12 +0900

    :bug: 상세페이지 북마크 숫자표기 오류 수정

--- src/app/(route)/detail/[postid]/page.tsx
+++ src/app/(route)/detail/[postid]/page.tsx
@@ -13,13 +13,22 @@ export default async function Detail({params} : {params : {postid : string}}) {
 
     const session = await getServerSession(authOptions) as any;
 
+    const res = await fetch(`${process.env.API_URL}/api/v1/meeting/${params.postid}`,{
+        headers : {
+            "Authorization" : session?.accessToken
+        },
+        cache : "no-store"
+    })
+
+    const detail = await res.json();
+
   return (
     <>
         <Headers/>
         <main className="pt-10 md:pt-[108px] pb-36 md:pb-40">
             <Wrap>
                 <Suspense fallback={<Skeleton/>}>
-                    <DetailUI postid={params.postid}/>
+                    <DetailUI detail={detail} postid={params.postid}/>
                     <Recommends/>
                     <Comments session={session} postid={params.postid}/>
                 </Suspense>
--- src/components/page/post/detail/DetailUI.tsx
+++ src/components/page/post/detail/DetailUI.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
@@ -7,6 +9,7 @@ import BtnList from "@/components/page/post/detail/BtnList";
 import Bookmark from "@/components/Common/Button/Bookmark/Bookmark";
 import moment from "moment";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 const ViewerComponent = dynamic(()=>import("@/components/page/post/detail/ViewerComponent"),{ssr : false});
 
@@ -41,16 +44,9 @@ const LOCATION : {[key : string] : string } = {
 const gridClass = 'grid items-center text-OnSurface text-xs md:text-lg font-medium gap-3 grid-cols-[62px_1fr] md:grid-cols-[102px_1fr]';
 const gridTitle = 'md:py-3 md:px-6 bg-Surface text-center box-border text-nowrap w-full h-8 md:h-10 flex items-center justify-center rounded'
 
-export default async function DetailUI({postid} : {postid : string}) {
+export default async function DetailUI({detail,postid} : {detail : any, postid : string}) {
 
-    const session = await getServerSession(authOptions) as any;
-    const res = await fetch(`${process.env.API_URL}/api/v1/meeting/${postid}`,{
-        headers : {
-            "Authorization" : session?.accessToken
-        },
-        cache : "no-store"
-    })
-    const data = await res.json();
+    const [bookCount,setBookCount] = useState(detail.data.bookmarkCount);
 
   return (
     <>
@@ -58,20 +54,22 @@ export default async function DetailUI({postid} : {postid : string}) {
             <Image src="/asset/image/icon/prev_arrow.svg" alt="뒤로가기" width={20} height={18}/>
         </Link>
         
-        <h1 className="text-lg md:text-2xl mt-1 md:mt-8 font-semibold leading-normal">{data.data.title}</h1>
+        <h1 className="text-lg md:text-2xl mt-1 md:mt-8 font-semibold leading-normal">{detail.data.title}</h1>
         
         <div className="flex items-center mt-2 md:mt-7 pb-[18px] text-sm md:text-lg font-medium text-OnBackgroundGray border-b border-Outline justify-between flex-wrap gap-1">
             <div className="flex items-center gap-[10px] md:gap-[18px]">
-                <p>{data.data.author !=="" ? data.data.author : "탈퇴한 유저"}</p>
-                <p>{moment(data.data.createdDateTime).format('YYYY-MM-DD')}</p>
+                <p>{detail.data.author !=="" ? detail.data.author : "탈퇴한 유저"}</p>
+                <p>{moment(detail.data.createdDateTime).format('YYYY-MM-DD')}</p>
                 <div className="flex items-center gap-[10px]">
                     <ViewSVG width={24} height={24} stroke="#A5A5A5"/>
-                    {data.data.viewCount}
+                    {detail.data.viewCount}
                 </div>
             </div>
             <div className="flex items-center gap-[6px] text-primary">
-                <Bookmark postId={data.data.postId} bookmarked={data.data.bookmarked}/>
-                <p className="text-sm md:text-xl font-medium">{data.data.bookmarkCount > 0 ? String(data.data.bookmarkCount).padStart(2,'0') : data.data.bookmarkCount}</p>
+                <Bookmark postId={Number(postid)} bookmarked={detail.data.bookmarked} setBookCount={setBookCount}/>
+                <p className="text-sm md:text-xl font-medium">
+                    {bookCount > 0 ? String(bookCount).padStart(2,'0') : bookCount}
+                </p>
             </div>
         </div>
         
@@ -79,56 +77,56 @@ export default async function DetailUI({postid} : {postid : string}) {
 
             <div className={gridClass}>
                 <h4 className={gridTitle}>모집형식</h4>
-                <p>{MEETINGTYPE[data.data.meetingType]}</p>
+                <p>{MEETINGTYPE[detail.data.meetingType]}</p>
             </div>
 
             <div className={gridClass}>
                 <h4 className={gridTitle}>지역</h4>
-                <p>{LOCATION[data.data.location]}</p>
+                <p>{LOCATION[detail.data.location]}</p>
             </div>
 
             <div className={gridClass}>
                 <h4 className={gridTitle}>구기종목</h4>
-                <p>{GAMETYPE[data.data.gameType]}</p>
+                <p>{GAMETYPE[detail.data.gameType]}</p>
             </div>
 
             {
-                data.data.meetingTime &&
+                detail.data.meetingTime &&
                 <div className={gridClass}>
                     <h4 className={gridTitle}>시간대</h4>
-                    <p>{data.data.meetingTime}</p>
+                    <p>{detail.data.meetingTime}</p>
                 </div>
             }
 
             {
-                data.data.meetingDays &&
+                detail.data.meetingDays &&
                 <div className={gridClass}>
                     <h4 className={gridTitle}>모임 요일</h4>
-                    <p>{data.data.meetingDays.split(';').join(',')}</p>
+                    <p>{detail.data.meetingDays.split(';').join(',')}</p>
                 </div>
             }
 
             {
-                data.data.meetingDateTime &&
+                detail.data.meetingDateTime &&
                 <div className={gridClass}>
                     <h4 className={gridTitle}>모임 날짜</h4>
-                    <p>{moment(data.data.meetingDateTime).format('YYYY-MM-DD')}</p>
+                    <p>{moment(detail.data.meetingDateTime).format('YYYY-MM-DD')}</p>
                 </div>
             }
             
             <div className={gridClass}>
                 <h4 className={gridTitle}>모집 인원</h4>
-                <p>{data.data.meetingMemberNum} 명</p>
+                <p>{detail.data.meetingMemberNum} 명</p>
             </div>
 
             <div className={gridClass}>
                 <h4 className={gridTitle}>모집 마감</h4>
-                <p>{data.data.meetingDeadline}</p>
+                <p>{detail.data.meetingDeadline}</p>
             </div>
 
             <div className="grid items-center text-OnSurface text-xs md:text-lg font-medium gap-3 grid-cols-[104px_1fr] md:grid-cols-[136px_1fr]">
                 <h4 className="py-3 px-6 bg-Surface text-center box-border text-nowrap w-full h-8 md:h-10 flex items-center justify-center rounded">오픈카톡 주소</h4>
-                <a href={data.data.openKakao} target="_blank" className="w-full md:w-[158px] h-8 md:h-10 flex items-center justify-center bg-primary text-white rounded whitespace-nowrap ">
+                <a href={detail.data.openKakao} target="_blank" className="w-full md:w-[158px] h-8 md:h-10 flex items-center justify-center bg-primary text-white rounded whitespace-nowrap ">
                     오픈톡 참여 <Image src="/asset/image/icon/link.svg" width={24} height={24} alt="링크 아이콘" />
                 </a>
             </div>
@@ -136,10 +134,10 @@ export default async function DetailUI({postid} : {postid : string}) {
         </div>
 
         <div className="mt-8 md:mt-24 min-h-72 md:min-h-[848px] w-full py-3 md:py-9 px-4 md:px-12 box-border text-sm md:text-lg font-medium leading-8 border">
-            <ViewerComponent content={data.data.content}/>
+            <ViewerComponent content={detail.data.content}/>
         </div>
 
-        <BtnList postid={postid} yours={data.data.yours} />
+        <BtnList postid={postid} yours={detail.data.yours} />
 
     </>
   )
